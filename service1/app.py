0890604852743a75f84ae6a10f355eea94b2fd27
Gabriel Park <40016456+gabrpark@users.noreply.github.com>
2024-03-24 14:52:19 -0400

    [Local machine] check-point before convert to blueprint-based

--- service1/app.py
+++ service1/app.py
@@ -3,59 +3,47 @@ import os
 from functools import wraps
 import firebase_admin
 from firebase_admin import credentials, auth
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, make_response, send_file
 # from flask_talisman import Talisman
 from flask_cors import CORS
 from flask_restful import Api, Resource
 from pymongo import MongoClient
+from config import firebase_config
+from auth import authenticate
 
-app = Flask(__name__, template_folder='./static')  # Create Flask app
+app = Flask(__name__)  # Create Flask app
 api = Api(app)  # Create API
 CORS(app)  # Enable CORS
 # Talisman(app)  # Enable HTTPS
 
-firebase_config = {
-    "type": os.environ.get("FIREBASE_TYPE"),
-    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace(r'\n', '\n'),
-    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
-    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL"),
-    "universe_domain": os.environ.get("FIREBASE_UNIVERSE_DOMAIN")
-}
-
 # Initialize Firebase Admin
-cred = credentials.Certificate(firebase_config)
-firebase_admin.initialize_app(cred)
+# cred = credentials.Certificate(firebase_config)
+# firebase_admin.initialize_app(cred)
 
 
-def authenticate(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        id_jwt_token = request.headers.get('Authorization')
-        if not id_jwt_token:
-            return {'status': 'error', 'message': 'Authorization token missing'}, 401
+# def authenticate(f):
+#     @wraps(f)
+#     def decorated_function(*args, **kwargs):
+#         id_jwt_token = request.headers.get('Authorization')
+#         if not id_jwt_token:
+#             return {'status': 'error', 'message': 'Authorization token missing'}, 401
 
-        try:
-            decoded_token = auth.verify_id_token(id_jwt_token, check_revoked=True)
-            request.user = decoded_token
-            return f(*args, **kwargs)
-        except auth.RevokedIdTokenError:
-            logging.error('Token revoked')
-            return {'status': 'error', 'message': 'Token has been revoked'}, 401
-        except auth.InvalidIdTokenError:
-            logging.error('Invalid token')
-            return {'status': 'error', 'message': 'Invalid token'}, 401
-        except Exception as e:
-            logging.error('Authentication error: %s', str(e))
-            return {'status': 'error', 'message': 'Authentication error'}, 500
+#         try:
+#             decoded_token = auth.verify_id_token(
+#                 id_jwt_token, check_revoked=True)
+#             request.user = decoded_token
+#             return f(*args, **kwargs)
+#         except auth.RevokedIdTokenError:
+#             logging.error('Token revoked')
+#             return {'status': 'error', 'message': 'Token has been revoked'}, 401
+#         except auth.InvalidIdTokenError:
+#             logging.error('Invalid token')
+#             return {'status': 'error', 'message': 'Invalid token'}, 401
+#         except Exception as e:
+#             logging.error('Authentication error: %s', str(e))
+#             return {'status': 'error', 'message': 'Authentication error'}, 500
 
-    #return decorated_function
-    return decorated_function 
+#     return decorated_function
 
 
 class Status(Resource):
@@ -72,27 +60,53 @@ class MongoDBResource(Resource):
             "MONGO_URI")).claspMobileDB
         super().__init__()
 
-    #@authenticate
+    @authenticate
     def get(self, collection_name):
         args = request.args.to_dict()
         collection = self.db[collection_name]
         documents = list(collection.find(args, {'_id': 0}))
         return jsonify(documents)
 
-    #@authenticate
+    @authenticate
     def post(self, collection_name):
         data = request.json
         collection = self.db[collection_name]
         result = collection.insert_one(data)
         return {'id': str(result.inserted_id)}
 
+
 class ApiDocs(Resource):
     def get(self):
-        return render_template('index.html')
+        html_content = render_template('docs.html')
+        response = make_response(html_content)
+        response.headers['Content-Type'] = 'text/html'
+        return response
+
+
+# Assuming your Flask app is launched from the 'service1' directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MEDIA_FOLDER = os.path.join(BASE_DIR, 'media-data')
+
+
+class MediaResource(Resource):
+    def get(self, task_dir, filename):
+        # Construct the full file path
+        file_path = os.path.join(MEDIA_FOLDER, task_dir, filename)
+
+        # Check if the file exists and is a file
+        if os.path.isfile(file_path):
+            return send_file(file_path)
+        else:
+            # Return a 404 not found response if the file doesn't exist
+            return "File not found", 404
+
+
+# Setup the route
+api.add_resource(MediaResource, '/media-data/<path:task_dir>/<filename>')
 
 api.add_resource(Status, '/status')
 api.add_resource(MongoDBResource, '/api/<collection_name>/')
-api.add_resource(ApiDocs, '/doc')
+api.add_resource(ApiDocs, '/docs')
 
 if __name__ == '__main__':
     # port = int(os.environ.get("PORT", 5000))
--- service1/auth.py
+++ service1/auth.py
@@ -1,23 +1,14 @@
 import logging
-from functools import wraps
-from firebase_admin import credentials, auth, initialize_app
-from flask import request
-# from config import firebase_config
 import os
-
-firebase_config = {
-    "type": os.environ.get("FIREBASE_TYPE"),
-    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace(r'\n', '\n'),
-    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
-    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL"),
-    "universe_domain": os.environ.get("FIREBASE_UNIVERSE_DOMAIN")
-}
+from functools import wraps
+import firebase_admin
+from firebase_admin import credentials, auth
+from flask import Flask, request, jsonify, render_template, make_response, send_file
+# from flask_talisman import Talisman
+from flask_cors import CORS
+# from flask_restful import Api, Resource
+from pymongo import MongoClient
+from config import firebase_config
 
 # Initialize Firebase Admin
 cred = credentials.Certificate(firebase_config)
